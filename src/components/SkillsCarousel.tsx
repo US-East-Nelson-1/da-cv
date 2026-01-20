c6c017cc4519068841bb64977a94ce5924301fd1
Nelson Guerra <nelsonguerra.tech@outlook.com>
2026-01-20 13:06:07 -0500

    add minor font weight change to the carousel

--- src/components/SkillsCarousel.tsx
+++ src/components/SkillsCarousel.tsx
@@ -13,7 +13,7 @@ export default function SkillsCarousel() {
     <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 animate-infinite-scroll">
         {skills.map((skill, index) => (
-          <li key={index} className="text-xs text-gray-700 whitespace-nowrap">
+          <li key={index} className="text-xs text-body text-gray-700 whitespace-nowrap">
             {skill}
           </li>
         ))}
